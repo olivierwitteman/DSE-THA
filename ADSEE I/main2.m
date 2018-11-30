5048700f5324cb73cb1de79b4afa95e83c3bbf02
S-PC\Stan <stanboekweit@gmail.com>
2018-11-30 11:46:10 +0100

    Update main2.m

--- ADSEE I/main2.m
+++ ADSEE I/main2.m
@@ -1,6 +1,6 @@
 clc; clear all
 % <------ INPUT    Means you can change/adjust it
-%hallo Stan
+%hallo Pieter
 %% Read reference aircraft for getting starting MTOW form average weights
 filename = 'Reference_Olivier.xlsx';    % reference file with aircraft
 sheet = 1;
