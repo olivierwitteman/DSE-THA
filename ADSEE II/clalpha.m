81ebf6ecfeee5f7b7bb4348dd768bd76c3755c3a
S-PC\Stan <stanboekweit@gmail.com>
2018-12-03 12:02:51 +0100

    update

--- ADSEE II/clalpha.m
+++ ADSEE II/clalpha.m
@@ -1,36 +1,8 @@
-<<<<<<< HEAD
-%input values for Cl-alpha curve for finite wing (clean and flapped
-%When using this matlab program, use of the slides of adsee 2 is prefferd
-%as it will help the user with certain specifics. Or Raymer for certain
-%values of figures. As this sizing is highly statistical and perliminary.
-%General inputs
-A=25;
-sweepc2 =1; 
-S=35;
-M=0.3;
-Beta=sqrt(1-M^2);
-eta=0.95;
-%%Airfoil ans wing
-sweepLE=0.25; %Calculate
-alpha0L=-9.; %Given by the airfoil
-CLdes=0.65; %Given
-taper=0.6; %Given
-Sharpfactor=0.02; %from the airfoil.
-clmax=1.2; %this is the clmax for the airfoil (slide 17)
-%%statistical
-%general
-C1=0.02; %From Table (slide:15)
-C2=0.02 ; %From table slide: 15 raymer.
-
-
-CLmax_base=0.2;
-%% Use the datcom method to get the CL clope from statistics
-=======
 function [CLmax, alpha_stall] = clalpha(A, clmax, sweepLE, CLdes)
 %input values for Cl-alpha curve for finite wing (clean and flapped
 %General inputs
 
-sweepc2 = 0.25; 
+sweepc2 = 0.25;
 Beta = 1;
 eta = 0.95;
 %%Airfoil ans wing
@@ -46,10 +18,9 @@ C2=0.02;  %From table slide: 15 raymer.
 
 CLmax_base=0.2;
 % Use the datcom method to get the CL slope from statistics
->>>>>>> ac1f3a0e02e78702f69a40724f8012ec5a134336
-Datcomtop=2*pi*A; 
+Datcomtop=2*pi*A;
 Datcombottom=sqrt(4+(A*Beta/eta)^2*(1+((tan(sweepc2))^2/Beta^2)))+2;
-CLalpha=Datcomtop/Datcombottom;  
+CLalpha=Datcomtop/Datcombottom;
 
 %next is the trim angle.this is the angle the aircraft needs to fly at to
 %fly at CLdes
@@ -58,34 +29,30 @@ alphatrim=CLdes/CLalpha+alpha0L;
 
 %Next is the CLmax, 2 methods are being introduced. The Datcom method and a
 %general  one. Datcom is prefferred. CLmax
-<<<<<<< HEAD
-Datcom_choose_method=((C1+1)*cos(sweepLE));
-=======
 
 Datcom_choose_method=((C1+1)*cos(sweepLE));
 
 delta_CLmax=0.5; %Term for M>0.2. Not for take-off and landing.
 delta_alpha_CLmax=0.02; %From table slide 19 (Raymer)
-CLM_clmax=0.05; %slide 17 use right table 
+CLM_clmax=0.05; %slide 17 use right table
 CLmax_base=0.2; %table from Raymer
 alpha_CLmax=0.04;
 
->>>>>>> ac1f3a0e02e78702f69a40724f8012ec5a134336
 if A> 4/Datcom_choose_method
     delta_CLmax_Datcom=0.5; %Term for M>0.2. Not for take-off and landing.
     delta_alpha_CLmax=0.02; %From table slide 19 (Raymer)
-    CLM_clmax=0.05; %slide 17 use right table 
+    CLM_clmax=0.05; %slide 17 use right table
     %high aspect ratio method
     CLmax=CLM_clmax*clmax+delta_CLmax_Datcom;
     alpha_stall=CLmax/CLalpha+alpha0L+delta_alpha_CLmax;
-%otherwise low aspect ratio method.    
+%otherwise low aspect ratio method.
 elseif A< 4/Datcom_choose_method
     CLmax_base=0.2; %table from Raymer
     delta_CLmax_Datcom=0.5;%table raymer for compressibility
     delta_alpha_CLmax=0.02; %table Raymer
     alpha_CLmax=0.04;%table Raymer
-    
-    CLmax=CLmax_base+delta_CLmax_Datcom; 
+
+    CLmax=CLmax_base+delta_CLmax_Datcom;
     alpha_stall=alpha_CLmax+delta_alpha_CLmax;
 end
 %% High Lift Devices.
@@ -108,7 +75,7 @@ Delta_Clmax_HLD=HLD(LE,TE,c_ac_c);
 
 HLD_Delta_CLmax=0.9*Delta_Clmax_HLD*Swf/S*cos(sweep_hinge); %The difference in CLmax
 %due to the high lift devices.
-HLD_Delta_alpha0L=delta_alpha0L_airfoil*Swf/S*cos(sweep_hinge); %Same but than 
+HLD_Delta_alpha0L=delta_alpha0L_airfoil*Swf/S*cos(sweep_hinge); %Same but than
 %0L angle of attack.
 %%
 %CLalpha for flapped configuration
@@ -121,6 +88,3 @@ end
 
 %Check if in the end the values make sense. Feasability check is very
 %important.
-
-
-
