02909d06cb5fa17cb60994194be5fe29ff30d639
Martin Georgiev <45416956+ThePuicho@users.noreply.github.com>
2019-01-07 15:20:15 +0100

    bj

--- Random folder/v54_Hybrid_V3/Main_tot_v3.m
+++ Random folder/v54_Hybrid_V3/Main_tot_v3.m
@@ -2,29 +2,29 @@ clc
 clear all
 close all
 tic
-AR = 10;
-Lambda = - 2.8;
-TR = 0.4;
-tc = 0.18;
-CLmax_Clean = 1.5;
-CLmax_TO = 1.7;
-CLmax_L = 2.1;
-
-
-% Oswald factor (no propulsive interaction assumed)
-e_clean=0.82;
-e_TO=0.77;
-e_L=0.73;
- 
-% zero-lift drag coefficient (no propulsive interaction assumed)
-CD0_clean=0.033;
-CD0_TO=0.068;
-CD0_L=0.098;
- 
-
-% Input_new(10)
-
-disp("Done")
+% AR = 10;
+% Lambda = - 2.8;
+% TR = 0.4;
+% tc = 0.18;
+% CLmax_Clean = 1.5;
+% CLmax_TO = 1.7;
+% CLmax_L = 2.1;
+% 
+% 
+% % Oswald factor (no propulsive interaction assumed)
+% e_clean=0.82;
+% e_TO=0.77;
+% e_L=0.73;
+%  
+% % zero-lift drag coefficient (no propulsive interaction assumed)
+% CD0_clean=0.033;
+% CD0_TO=0.068;
+% CD0_L=0.098;
+%  
+% 
+% % Input_new(10)
+% 
+% disp("Done")
 
 
 
@@ -247,7 +247,7 @@ close all
 V_cruise = m.cr.v;
 h = m.cr.h;
 S = Sw_roelof;
-A = AR;
+A = a.AR;
 frac = m.cr.f;
 m_cruise = MTOW * frac
 
@@ -279,7 +279,7 @@ summary_wing = [summary_wing; ["Wing Area", S]];
 
 % P = double(vars.P); % FIND IT FROM ROELOF
 P = WPdes.minGTM.GTM; % ADD THE SECONDARY POINT WHICH WE DO NOT KNOW for some reason
-A = AR
+A = a.AR
 MAC = double(summary_wing(11,2));           % <---- CHANGE FOR ELECTRIC/HYBRID
 OEW = M.OEM + M.EM1 + M.EM2 + M.bat + M.f + M.GT + M.w;
 S_ref = S;                        % <---- FROM ROELOF
