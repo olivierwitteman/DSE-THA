afaa1e932cd337149f4e690c734225ffd85cc994
Roel <roel3005@hotmail.nl>
2019-01-17 09:13:44 +0100

    performance

--- performance/Maximum_ROC.m
+++ performance/Maximum_ROC.m
@@ -7,39 +7,85 @@ lambda_isa=-0.0065;
 P0_isa=101325;
 T_0_isa=288.15;
 g_isa=9.80665;
-R_isa=287
+R_isa=287;
 %other
+h2=2400;
+h1=15.24;
 A=10;
 e=0.8;
 S=8.8;
 W=1700*9.80665;
-PA=250000;
+%important
+PA=240000;
+%
 rho=1.225;
 CD0=0.0388;
-CD0_takeoff=0.0377+0.015;
+CD0_takeoff=CD0+0.015;
+CD0_land=CD0+0.055;
 CL=sqrt(3*pi*A*e*CD0_takeoff);
 CD=CD0_takeoff+CL^2/(pi*A*e);
-max=CL^(3/2)/CD;
+max5=CL^(3/2)/CD;
 h=linspace(0,2400)
 ROC_mat=[]
 for i =h
 T=288.15+lambda_isa*i;
 P_isa = P0_isa * ((1 + (lambda_isa*i) / T_0_isa) ^ (-g_isa / (R_isa*lambda_isa)));
 rho_isa=P_isa/R_isa/T;
-ROC=PA/W-sqrt(W/S)*sqrt(2)/(max*sqrt(rho_isa));
+ROC=PA/W-sqrt(W/S)*sqrt(2)/(max5*sqrt(rho_isa));
 ROC_mat=[ROC_mat, ROC];
 end
-plot(h,ROC_mat)
+disp(max(ROC_mat))
+%plot(h,ROC_mat)
 %% Minimum rate of Descent
-ROD=-sqrt(W/S)*sqrt(2)/(max*sqrt(rho))
+ROD=-sqrt(W/S)*sqrt(2)/(max5*sqrt(rho))
 
 
 %% Time to climb
 V_stall=32.5;
 V_takeoff=1.2*V_stall;
 V_cruise=92.6;
-He1=15.24+(V_takeoff^2/(2*g_isa));
-He2=2400+(V_cruise^2/(2*g_isa));
+He1=h1+V_takeoff^2/(2*g_isa);
+He2=h2++(V_cruise^2/(2*g_isa));
 
 ROC_avg=(ROC_mat(1)+ROC_mat(100))/2;
-t_climb=(He2-He1)/ROC_avg
+t_climb=(He2-He1)/ROC_avg;
+t_climb1=(2400-15.24)/ROC_avg;
+
+
+
+%% maximum climb angle.
+
+V_theata= 4*W/S*1/(pi*A*e)/(1.225*0.8*(PA/W));
+PRmin=W*sqrt(W/S*2/rho_isa*CD^2/CL^3);
+Vcl=sqrt(W/S*2/rho_isa*1/CL);
+max_angle=(PA-PRmin)/(W*Vcl)*180/pi
+
+
+%% horizontal distance during climb and descent
+normal_angle= (max_angle-5)/180*pi;
+save('normal_angle')
+s_horizontal=(h2-h1)/tan(normal_angle)
+s_horizontal2=(He2-He1)/tan(normal_angle)
+
+%% minimum glide angle
+CL_glide= sqrt(CD0_land*A*pi*e);
+CD_glide= CD0_land+CL_glide^2/(pi*A*e);
+glide_angle=asin(-1/(CL/CD))*180/pi
+
+%% MAximum speed
+V=linspace(0,200,1000);
+prmat=[];
+ 
+for i=V
+    CL=W./S*2./rho_isa*1./(i^2);
+    CD=CD0+CL.^2/(pi*A*e);
+    PR=W*sqrt(W./S*2/rho_isa*CD.^2./CL.^3);
+    prmat=[prmat, PR];
+    if PR<1.001*PA && PR>0.999*PA % || PR<0.95*PA
+        V_max=i
+    end
+end
+
+plot(V,prmat)
+line([0 200],[PA PA])
+
--- performance/airfield_perf.m
+++ performance/airfield_perf.m
@@ -1,37 +1,47 @@
 %%% AIRFIELD TO PERFORMANCE
+clear all
+clear clc
 
 %% Assumptions
 
 %% TAKE OFF
 %% INPUTS
-W_to=1667*9.81          ;                                       %%INPUT 
-S=12.1                  ;                                       %%INPUT 
+W_to=1300*9.81          ;                                       %%INPUT 
+S=8.8                  ;                                       %%INPUT 
 rho=1.225               ;                                       %%INPUT    
 Cl_max=2.               ;                                       %%INPUT 
-Cd_to=0.505             ;                                       %%INPUT
-g=9.81                  ;                                       %%INPUT    
-T_av_to=1               ;     341kW                             %%INPUT                                           %%INPUT 
+Cd_to=0.0388+0.015            ;                                       %%INPUT
+g=9.80665                 ;                                       %%INPUT    
+T_av_to=10000               ;   %  341kW                             %%INPUT                                           %%INPUT 
 Dg_av_to=1              ;                                       %%INPUT 
-gamma_to=alpha             ;                                       %%INPUT                  
-H_scr=15.24             ;%[M]  %input from guide                %%INPUT
-
+gamma_to= load('normal_angle.mat')     ;    %%INPUT  
+gamma_to=gamma_to.max_angle*pi/180;
+H_scr=15.24         ;          	%M]  %input from guide                %%INPUT
+mu = 0.02           ;
+W=1300*g   ;
+Cl=1.7;
+Vlimit=sqrt(W/S*2/rho*1/Cl);
+V=linspace(0,Vlimit);
+Dg=mu*(W-Cl*0.5*rho*V.^2*S);
+DG=mean(Dg);
 %% Calculations
 %Ground distance
-V_lof = 1.05*V_min
+V_min = sqrt(2*W_to/(S*rho*Cl_max));
 
-V_min = sqrt(2*W_to/(S*rho*Cl_max))
+V_lof = 1.05*V_min;
 
-D_av_to = 0.5 * rho * S * Cd_to * (V_lof/sqrt(2))^2              
+D_av_to = 0.5 * rho * S * Cd_to * (V_lof/sqrt(2))^2  ;            
 
-a_av = (g/W_to)*(T_av_to-D_av_to-Dg_av_to)
+a_av = (g/W_to)*(T_av_to-D_av_to-DG);
 
-X_gd_to = V_lof^2/(2*a_av)
+X_gd_to = V_lof^2/(2*a_av);
 
 % Transition phase
-X_tp_to = V_lof^2*sin(gamma_to)/(0.15*g)
+X_tp_to = V_lof^2*sin(gamma_to)/(0.15*g);
 
 % Climb distance
-X_cd_to = H_scr-(1-cos((gamma_to)*(V_lof^2/(0.15*g))))/tan(gamma_to)
+X_cd_to = (H_scr-(1-cos(gamma_to))*V_lof^2/(0.15*g))/tan(gamma_to);
+
 
 % TO total distance
 X_TO = X_gd_to + X_tp_to + X_cd_to
@@ -41,33 +51,33 @@ X_TO = X_gd_to + X_tp_to + X_cd_to
 %% LANDING
 %% INPUTS
 
-W_la = 1                ;                                       %%INPUT
-T_av_la = 1             ;                                       %%INPUT
-D_av_la = 1             ;                                       %%INPUT
-Dg_av_la = 1            ;                                       %%INPUT
+W_la = 1000*g               ;                                       %%INPUT
+T_av_la = 2000             ;                                       %%INPUT
+D_av_la = 3000             ;                                       %%INPUT
+
 gamma_la = 1            ;                                       %%INPUT
 Trans_time = 2          ; %[sec] time to transition             %%INPUT
-mu = 1                  ;                                       %%INPUT
-delta_n = 0.15*9.81     ; %[] 0.15*g for prop                   %%INPUT
+mu = 0.02                  ;                                       %%INPUT
+delta_n = 0.15*g     ; %[] 0.15*g for prop                   %%INPUT
 T_av_rev = 1            ;                                       %%INPUT
-L_av_la = 1             ;                                       %%INPUT
-
+L_av_la = 1             ; 
+Cl_land=2.0             ;
+V_ap = 1.3*V_min;%%INPUT
+V_land=linspace(V_ap, 0,500);
 %% Calculations
+DG_land=mu*(W_la-0.5*rho*S*V_land.^2*Cl_land);
+DG_la=DG_land(DG_land>=0);
+DG_la=mean(DG_la)
+R = 1.3^2*W_la*2/(S*rho*Cl_max*delta_n*g);
 
-V_ap = 1.3*V_min*Cl_max*(delta_n*g)
-
-% Airborne distance
-
-R = 1.3^2*W_la*2/(S*rho*Cl_max*delta_n*g)
-
-X_ab_la = R * sin(gamma_la) + (H_scr-(1-cos(gamma_la))*R/tan(gamma_la))
+X_ab_la = R * sin(gamma_la) + (H_scr-(1-cos(gamma_la))*R)/tan(gamma_la);
 
 % Transition phase
-X_tp_la = Trans_time * V_ap
+X_tp_la = Trans_time * V_ap;
 
 % Braking phase
 
-X_br_la = W_la^2*2*1.3^2/(2*g*S*rho*Cl_max*(T_av_rev+D_av_la + mu*(W_la-L_av_la)))
+X_br_la = W_la^2*2*1.3^2/(2*g*S*rho*Cl_max*(T_av_rev+D_av_la + DG_la));
  
 X_LA = X_br_la + X_tp_la + X_ab_la
 
