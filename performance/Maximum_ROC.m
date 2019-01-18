4e0e077442e7a5a17dd9ab3b0327afe28b03258d
Roel <roel3005@hotmail.nl>
2019-01-18 09:16:48 +0100

    jajaj

--- performance/Maximum_ROC.m
+++ performance/Maximum_ROC.m
@@ -5,48 +5,54 @@ clc
 %ISA
 lambda_isa=-0.0065;
 P0_isa=101325;
-T_0_isa=288.15
-h2=2400;;
+T_0_isa=288.15;
+rho_0_isa=1.225;
+h2=2400;
 g_isa=9.80665;
 R_isa=287;
 %other
 h1=15.24;
 A=10;
-e_clean=0.8;
+e_clean=0.82;
 e_landing=0.73;
 e_takeoff=0.77;
-
+W_to=1434.6*9.80665;
 S=10.23;
-W=1.5425*10^3*9.80665;
+W=1395*9.80665;
 %important
-PA=290000;
+PA_total=180000;
+PA_secondary= 30000;
+PA_primary=150000
 %
 CL_takeoff=1.7;
-CL_land=2.1;
+CL_land=2.;
 rho=1.225;
-CD0=0.0388;
-CD0_takeoff=CD0+0.015;
-CD0_land=CD0+0.055;
+CD0=0.032;
+CD0_takeoff=0.0470;
+CD0_land=0.0870;
 CL=sqrt(3*pi*A*e_takeoff*CD0_takeoff);
-CD=CD0_takeoff+CL^2/(pi*A*e_clean);
+CD=CD0_takeoff+CL^2/(pi*A*e_takeoff);
 max5=CL^(3/2)/CD;
-h=linspace(0,2400);
+h=linspace(0,3880,5000);
 ROC_mat=[];
+rc=(110-190)*0.75*1000/((15000-2500)*0.3048);
 for i =h
 T=288.15+lambda_isa*i;
 P_isa = P0_isa * ((1 + (lambda_isa*i) / T_0_isa) ^ (-g_isa / (R_isa*lambda_isa)));
 rho_isa=P_isa/R_isa/T;
-ROC=PA/W-sqrt(W/S)*sqrt(2)/(max5*sqrt(rho_isa));
+PA1=rho_isa/rho_0_isa*PA_total+rc*i;
+ROC=PA1/W_to-sqrt(W_to/S)*sqrt(2)/(max5*sqrt(rho_isa));
 ROC_mat=[ROC_mat, ROC];
 end
 %disp(max(ROC_mat))
 plot(h,ROC_mat)
-%% Minimum rate of Descent
-ROD=-sqrt(W/S)*sqrt(2)/(max5*sqrt(rho))
+%% Minimum rate of Descent (T=0)
+W_land=1336*9.80665;
+ROD=-sqrt(W_land/S)*sqrt(2)/(max5*sqrt(rho))
 
 
 %% Time to climb
-V_stall=sqrt(W/S*2/rho_isa*1/CL_takeoff);
+V_stall=sqrt(W_to/S*2/rho_isa*1/CL_takeoff);
 V_takeoff=1.2*V_stall;
 V_cruise=92.6;
 He1=h1+V_takeoff^2/(2*g_isa);
@@ -60,22 +66,22 @@ t_climb1=(2400-15.24)/ROC_avg;
 
 %% maximum climb angle.
 
-V_theata= 4*W/S*1/(pi*A*e_takeoff)/(1.225*(PA/W));
-PRmin=W*sqrt(W/S*2/rho_isa*CD^2/CL^3);
-Vcl=sqrt(W/S*2/rho_isa*1/CL);
-max_angle=(PA-PRmin)/(W*Vcl)*180/pi
+V_theata= 4*W_to/S*1/(pi*A*e_takeoff)/(1.225*(PA_total/W_to));
+PRmin=W_to*sqrt(W_to/S*2/rho*CD^2/CL^3);
+Vcl=sqrt(W_to/S*2/rho*1/CL_takeoff);
+max_angle=asin((PA_total-PRmin)/(W_to*Vcl))*180/pi;
 
 
 %% horizontal distance during climb and descent
-normal_angle= (max_angle-5)/180*pi;
+normal_angle= (max_angle-6)/180*pi;
 save('normal_angle')
-s_horizontal=(h2-h1)/tan(normal_angle)
-s_horizontal2=(He2-He1)/tan(normal_angle)
+s_horizontal=(h2-h1)/tan(normal_angle);
+s_horizontal2=(He2-He1)/tan(normal_angle);
 
 %% minimum glide angle
 CL_glide= sqrt(CD0_land*A*pi*e_landing);
 CD_glide= CD0_land+CL_glide^2/(pi*A*e_landing);
-glide_angle=asin(-1/(CL/CD))*180/pi
+glide_angle=asin(-1/(CL_glide/CD_glide))*180/pi
 
 %% MAximum speed
 V=linspace(0,200,10000);
@@ -86,7 +92,7 @@ for i=V
     CD=CD0+CL.^2/(pi*A*e_clean);
     PR=W*sqrt(W./S*2/rho_isa*CD.^2./CL.^3);
     prmat=[prmat, PR];
-    if PR<1.001*PA && PR>0.999*PA % || PR<0.95*PA
+    if PR<1.001*PA_total && PR>0.999*PA_total % || PR<0.95*PA
         V_max=i;
     end
 end
--- performance/airfield_perf.m
+++ performance/airfield_perf.m
@@ -6,81 +6,85 @@ clear clc
 
 %% TAKE OFF
 %% INPUTS
-W_to=1300*9.81          ;                                       %%INPUT 
-S=8.8                  ;                                       %%INPUT 
+W_to=1434.6*9.81          ;                                       %%INPUT 
+S=9.529 ; 
+PA=180000;                                       %%INPUT 
 rho=1.225               ;                                       %%INPUT    
 %Cl_max=2.               ;                                       %%INPUT 
-Cd_to=0.0388+0.015            ;                                       %%INPUT
+Cd_to=0.0470            ;                                       %%INPUT
 g=9.80665                 ;                                       %%INPUT    
-T_av_to=10000               ;   %  341kW                             %%INPUT                                           %%INPUT 
-Dg_av_to=2000             ;                                       %%INPUT 
+
+
 gamma_to= load('normal_angle.mat')     ;    %%INPUT  
-gamma_to=gamma_to.max_angle*pi/180;
+gamma_to1=gamma_to.normal_angle;
 H_scr=15.24         ;          	%M]  %input from guide                %%INPUT
-mu = 0.02           ;
-W=1300*g   ;
+mu = 0.02          ;
+
 Cl=1.7;
 A=10;
 e_takeoff=0.77;
-CD_land=0.0388+0.015+Cl^2/(pi*A*e_takeoff);
-Vlimit=sqrt(W/S*2/rho*1/Cl);
+
+CD_to=Cd_to+Cl^2/(pi*A*e_takeoff)
+Vlimit=sqrt(W_to/S*2/rho*1/Cl);
 V=linspace(0,Vlimit);
-Dg=mu*(W-Cl*0.5*rho*V.^2*S);
-DG=mean(Dg);
-D=0.5*rho*V.^2*S*CD_land;
-D_av_to=mean(D);
+
+Dg=mu*(W_to-Cl*0.5*rho*V.^2*S);
+DG=(Dg(1)+Dg(100));
+%D=0.5*rho*V.^2*S*CD_to;
+%D_av_to=mean(D)
 %% Calculations
 %Ground distance
 V_min = sqrt(2*W_to/(S*rho*Cl));
 
 V_lof = 1.05*V_min;
-
-D_av_to = 0.5 * rho * S * Cd_to * (V_lof/sqrt(2))^2  ;            
-
+T_av_to=PA/(V_lof*0.8);
+D_av_to = 0.5 * rho * S * (Cd_to+Cl^2/(pi*A*e_takeoff)) * (V_lof/sqrt(2))^2              
+DG=(W_to-0.5*rho*S*Cl*(V_lof/sqrt(2))^2)*mu
 a_av = (g/W_to)*(T_av_to-D_av_to-DG);
 
 X_gd_to = V_lof^2/(2*a_av);
 
 % Transition phase
-X_tp_to = V_lof^2*sin(gamma_to)/(0.15*g);
+X_tp_to = V_lof^2*sin(gamma_to1)/(0.15*g);
 
 % Climb distance
-X_cd_to = (H_scr-(1-cos(gamma_to))*V_lof^2/(0.15*g))/tan(gamma_to);
-
+X_cd_to = (H_scr-(1-cos(gamma_to1))*V_lof^2/(0.15*g))/tan(gamma_to1)
+X_cd_to=(H_scr/tan(gamma_to1))
 
 % TO total distance
-X_TO = X_gd_to + X_tp_to + X_cd_to;
+X_TO = X_gd_to + X_tp_to + X_cd_to
 
 
 
 %% LANDING
 %% INPUTS
 
-W_la = 1000*g               ;                                       %%INPUT
-T_av_la = 2000             ;
+W_la = 1384.6*g              ;                                       %%INPUT
+
 Cl_land=2.0  ; %%INPUT
 V_ap = 1.3*V_min;%%INPUT
+T_av_rev=PA/(V_ap*0.4)
 V_land=linspace(V_ap, 0,500);
-e_landing=0.72;
+e_landing=0.73;
 CD=0.0388+0.055+Cl_land^2/(pi*A*e_landing);
 D_land=0.5*rho*S*V_land.^2*CD;
 D_av_la=mean(D_land);
 
 
-gamma_la = 1            ;                                       %%INPUT
-Trans_time = 2          ; %[sec] time to transition             %%INPUT
-mu = 0.02                  ;                                       %%INPUT
-delta_n = 0.15*g     ; %[] 0.15*g for prop                   %%INPUT
-T_av_rev = 1            ;                                       %%INPUT
-L_av_la = 1             ; 
+gamma_la = 3*pi/180            ;                                       %%INPUT
+Trans_time = 3          ; %[sec] time to transition             
+mu = 0.05                  ;                                    
+delta_n = 0.15 *g    ; %[] 0.15*g for                                       %%INPUT
+
 Cl_land=2.0             ;
-V_ap = 1.3*V_min;%%INPUT
+
 
 %% Calculations
 DG_land=mu*(W_la-0.5*rho*S*V_land.^2*Cl_land);
 DG_la=DG_land(DG_land>=0);
 DG_la=mean(DG_la);
-R = 1.3^2*W_la*2/(S*rho*Cl_land*delta_n*g);
+%DG_la=mu*W_la
+R = 1.3^2*sqrt(W_la*2/(S*rho*Cl_land))/(delta_n*g);
 
 X_ab_la = R * sin(gamma_la) + (H_scr-(1-cos(gamma_la))*R)/tan(gamma_la);
 
